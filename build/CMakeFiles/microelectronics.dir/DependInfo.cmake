4a90bf8f87b624998059ef6604a04a7cbf1ab4e1
Dunedain117 <andreadonadios@gmail.com>
2025-10-29 10:01:39 +0100

    fist

---
+++ build/CMakeFiles/microelectronics.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Andrea/Desktop/simulazioni/microelectronics_mod/microelectronics.cc" "CMakeFiles/microelectronics.dir/microelectronics.cc.o" "gcc" "CMakeFiles/microelectronics.dir/microelectronics.cc.o.d"
+  "/mnt/c/Users/Andrea/Desktop/simulazioni/microelectronics_mod/src/ActionInitialization.cc" "CMakeFiles/microelectronics.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/microelectronics.dir/src/ActionInitialization.cc.o.d"
+  "/mnt/c/Users/Andrea/Desktop/simulazioni/microelectronics_mod/src/DetectorConstruction.cc" "CMakeFiles/microelectronics.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/microelectronics.dir/src/DetectorConstruction.cc.o.d"
+  "/mnt/c/Users/Andrea/Desktop/simulazioni/microelectronics_mod/src/DetectorMessenger.cc" "CMakeFiles/microelectronics.dir/src/DetectorMessenger.cc.o" "gcc" "CMakeFiles/microelectronics.dir/src/DetectorMessenger.cc.o.d"
+  "/mnt/c/Users/Andrea/Desktop/simulazioni/microelectronics_mod/src/ElectronCapture.cc" "CMakeFiles/microelectronics.dir/src/ElectronCapture.cc.o" "gcc" "CMakeFiles/microelectronics.dir/src/ElectronCapture.cc.o.d"
+  "/mnt/c/Users/Andrea/Desktop/simulazioni/microelectronics_mod/src/G4ElectronCapture.cc" "CMakeFiles/microelectronics.dir/src/G4ElectronCapture.cc.o" "gcc" "CMakeFiles/microelectronics.dir/src/G4ElectronCapture.cc.o.d"
+  "/mnt/c/Users/Andrea/Desktop/simulazioni/microelectronics_mod/src/MicroElecPhysics.cc" "CMakeFiles/microelectronics.dir/src/MicroElecPhysics.cc.o" "gcc" "CMakeFiles/microelectronics.dir/src/MicroElecPhysics.cc.o.d"
+  "/mnt/c/Users/Andrea/Desktop/simulazioni/microelectronics_mod/src/MicroElecSiPhysics.cc" "CMakeFiles/microelectronics.dir/src/MicroElecSiPhysics.cc.o" "gcc" "CMakeFiles/microelectronics.dir/src/MicroElecSiPhysics.cc.o.d"
+  "/mnt/c/Users/Andrea/Desktop/simulazioni/microelectronics_mod/src/PhysicsList.cc" "CMakeFiles/microelectronics.dir/src/PhysicsList.cc.o" "gcc" "CMakeFiles/microelectronics.dir/src/PhysicsList.cc.o.d"
+  "/mnt/c/Users/Andrea/Desktop/simulazioni/microelectronics_mod/src/PrimaryGeneratorAction.cc" "CMakeFiles/microelectronics.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/microelectronics.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/mnt/c/Users/Andrea/Desktop/simulazioni/microelectronics_mod/src/RunAction.cc" "CMakeFiles/microelectronics.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/microelectronics.dir/src/RunAction.cc.o.d"
+  "/mnt/c/Users/Andrea/Desktop/simulazioni/microelectronics_mod/src/SteppingAction.cc" "CMakeFiles/microelectronics.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/microelectronics.dir/src/SteppingAction.cc.o.d"
+  "/mnt/c/Users/Andrea/Desktop/simulazioni/microelectronics_mod/src/TrackingAction.cc" "CMakeFiles/microelectronics.dir/src/TrackingAction.cc.o" "gcc" "CMakeFiles/microelectronics.dir/src/TrackingAction.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
